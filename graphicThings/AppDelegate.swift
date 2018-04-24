87f4027d67c9141ba738eb6263a3a1a6887e2751
daveyj <daveyj@purdue.edu>
2018-04-24 12:41:31 -0400

    Initial Commit

---
+++ graphicThings/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  graphicThings
+//
+//  Created by Joseph Davey on 4/24/18.
+//  Copyright © 2018 Joseph Davey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ graphicThings/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  graphicThings
+//
+//  Created by Joseph Davey on 4/24/18.
+//  Copyright © 2018 Joseph Davey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
